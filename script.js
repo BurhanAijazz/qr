17c8a507db202d9129375a0135d1d448927bd946
MuzzammilRafiq <mzmlrafiqcse@gmail.com>
2023-07-26 12:59:26 +0530

    fixed error in regex

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let qrImg = document.getElementById("qr-img");
 let qr_text = document.getElementById("qr-text");
 function generateQR() {
     qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qr_text.value;
-    var expression =(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,});
+    var expression ='https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}';
     var regex = new RegExp(expression);
     var t = qr_text.value;
     if (t.match(regex)) {
